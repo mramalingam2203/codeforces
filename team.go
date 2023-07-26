40381b1442ee60c9ce4bd33915c3d901b045a3d8
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-27 01:14:32 +0530

    code init

---
+++ team.go
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/231/A
+
+package main
+
+func main() {
+
+	fmt.Scanf("%d", &n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d %d %d", &petya, &vasya, &tonya)
+	}
+}

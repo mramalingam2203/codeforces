4c115c45062d7fb989c10ff45527dd15da796e46
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-27 01:27:03 +0530

    logic works

--- team.go
+++ team.go
@@ -2,11 +2,32 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	var numRows int
+	fmt.Scanf("%d", &numRows)
 
-	fmt.Scanf("%d", &n)
+	numCols := 3 // Petya, Vasya, Tonya scores
+	scores := make([][]int, numRows)
 
-	for i := 0; i < n; i++ {
-		fmt.Scanf("%d %d %d", &petya, &vasya, &tonya)
+	for i := range scores {
+		scores[i] = make([]int, numCols)
+	}
+
+	count := 0
+	for i := 0; i < numRows; i++ {
+		fmt.Scanf("%d %d %d", &scores[i][0], &scores[i][1], &scores[i][2])
+		if scores[i][0] != 0 || scores[i][0] != 1 || scores[i][1] != 0 || scores[i][1] != 1 || scores[i][2] != 0 || scores[i][2] != 1 {
+
+			fmt.Println("Invalid")
+			os.Exit(0)
+		}
+		if scores[i][0]+scores[i][1]+scores[i][2] >= 2 {
+			count++
+		}
 	}
 }

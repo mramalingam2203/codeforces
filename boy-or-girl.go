4d9c3df041c076d045e9c164ccf43f046ac8895f
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-27 19:25:44 +0530

    find boy of girl with login name

---
+++ boy-or-girl.go
@@ -0,0 +1,17 @@
+// https://codeforces.com/problemset/problem/236/A
+
+package main
+
+func main() {
+	fmt.Scanf("%s", word)
+
+}
+
+func boyOrGirl(name string) {
+	hash : = make(map[[byte]int],0)
+	for i := range name {
+
+
+	}
+
+}

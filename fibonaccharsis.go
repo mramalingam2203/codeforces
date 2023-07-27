6b5382271133836ce9b524ba3e969ff4ce30de39
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-27 12:29:44 +0530

    check fibo functions works alright

--- fibonaccharsis.go
+++ fibonaccharsis.go
@@ -7,38 +7,41 @@ import (
 )
 
 func main() {
-	// read number of test cases
-	var numRows int
-	fmt.Scanf("%d", &numRows)
+	// // read number of test cases
+	// var numRows int
+	// fmt.Scanf("%d", &numRows)
 
-	numCols := 2 // Petya, Vasya, Tonya scores
-	numbers := make([][]int, numRows)
+	// numCols := 2 // Petya, Vasya, Tonya scores
+	// numbers := make([][]int, numRows)
 
-	for i := range numbers {
-		numbers[i] = make([]int, numCols)
-	}
+	// for i := range numbers {
+	// 	numbers[i] = make([]int, numCols)
+	// }
 
-	for i := 0; i < numRows; i++ {
-		fmt.Scanf("%d %d", &numbers[i][0], &numbers[i][1])
-	}
+	// for i := 0; i < numRows; i++ {
+	// 	fmt.Scanf("%d %d", &numbers[i][0], &numbers[i][1])
+	// }
 
-	fmt.Println(numbers)
-	
-	yesFib := [5]int {}
+	// fmt.Println(numbers)
+
+	Fib1 := fibonacciSequence(5)
+	Fib2 := fibonacciSequence(6)
+	noFib := []int{6, 8, 14, 23}
+
+	fmt.Println(Fib1, Fib2)
+	fmt.Println(checkFibonacci(Fib1), checkFibonacci(Fib2), checkFibonacci(noFib))
 
 }
 
-func checkFibonacci(numbers []float64) bool {
+func checkFibonacci(numbers []int) bool {
 
-	for i := range numbers {
-		for i > 2 {
-			if !numbers[i] == numbers[i-1]+numbers[i-2] {
-				return false
-			}
+	for i := 2; i < len(numbers); i++ {
+		if !(numbers[i] == numbers[i-1]+numbers[i-2]) {
+			return false
 		}
 	}
 
-
+	return true
 }
 
 func fibonacciSequence(length int) []int {
@@ -54,6 +57,4 @@ func fibonacciSequence(length int) []int {
 	}
 
 	return sequence
-}
-	return true
 }
